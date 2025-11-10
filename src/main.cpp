516257d6797d9a3dee23a6bcd0fdee798104dd96
Sam Caldwell <github@samcaldwell.net>
2025-11-10 09:33:03 -0600

    refactor main()

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
  */
 #include <iostream>
 #include <sstream>
-#include <string>
 #include <vector>
 
 #include "pycc/driver/cli.h"
@@ -29,34 +28,34 @@
 
 using pycc::driver::CliOptions;
 
-int main(int argc, char** argv) {
+auto main(const int argc, char **argv) -> int {
     try {
-    using pycc::driver::ParseCli;
-    using pycc::driver::PrintUsage;
-    CliOptions opts;
-    if (!ParseCli(argc, (const char* const*)argv, opts, std::cerr)) {
-        PrintUsage(std::cerr, argv[0]); // NOLINT(*-pro-bounds-pointer-arithmetic)
-        return 2;
-    }
-    if (opts.show_help) {
-        PrintUsage(std::cout, argv[0]); // NOLINT(*-pro-bounds-pointer-arithmetic)
-        return 0;
-    }
-    pycc::metrics::Metrics::Enable(opts.metrics);
-    if (opts.inputs.size() != 1) { std::cerr << "pycc: error: exactly one input file is supported in MVP" << '\n'; return 2; }
-    const int ret_code = pycc::driver::CompileOnce(opts, opts.inputs[0]);
-    pycc::driver::ReportMetricsIfRequested(opts);
-    return ret_code;
-    }
-    catch (const pycc::exceptions::PyccException& ex) {
+        using pycc::driver::ParseCli;
+        using pycc::driver::PrintUsage;
+        CliOptions opts;
+        if (!ParseCli(argc, (const char * const*) argv, opts, std::cerr)) {
+            PrintUsage(std::cerr, argv[0]); // NOLINT(*-pro-bounds-pointer-arithmetic)
+            return 2;
+        }
+        if (opts.show_help) {
+            PrintUsage(std::cout, argv[0]); // NOLINT(*-pro-bounds-pointer-arithmetic)
+            return 0;
+        }
+        pycc::metrics::Metrics::Enable(opts.metrics);
+        if (opts.inputs.size() != 1) {
+            std::cerr << "pycc: error: exactly one input file is supported in MVP" << '\n';
+            return 2;
+        }
+        const int ret_code = pycc::driver::CompileOnce(opts, opts.inputs[0]);
+        pycc::driver::ReportMetricsIfRequested(opts);
+        return ret_code;
+    } catch (const pycc::exceptions::PyccException &ex) {
         std::cerr << "pycc: " << ex.what() << '\n';
         return 2;
-    }
-    catch (const std::exception& ex) {
+    } catch (const std::exception &ex) {
         std::cerr << "pycc: internal error: " << ex.what() << '\n';
         return 2;
-    }
-    catch (...) {
+    } catch (...) {
         std::cerr << "pycc: unknown error" << '\n';
         return 2;
     }
